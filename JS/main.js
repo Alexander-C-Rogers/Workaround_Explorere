607040b68265ec741a19adec1b3d98e77847750f
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-08 12:57:13 -0400

    Create input buttons for every company and role represented in the data.

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,9 @@
 import {getRoles, getCompanies} from './modules/salaryData.js';
 import {getAverageSalaryByRole, getSalaryAtCompany, getIndustryAverageSalary, getAverageSalaryByCompany} from './modules/workAroundModule.js';
 import formatNumbers from './modules/utilities.js';
+const companies = getCompanies();
+const roles = getRoles();
+
+// Create input buttons for every company and role represented in the data.
+renderInputButtons(companies, 'company');
+renderInputButtons(roles, 'role');
