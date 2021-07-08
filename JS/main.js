81e535e745ac10079d30f0ac7cfdafd8c37fe8ad
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-08 12:52:21 -0400

    import workAroundModule.js to main.js and link main.js to index.html

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,2 @@
 import {getRoles, getCompanies} from './modules/salaryData';
+import {getAverageSalaryByRole, getSalaryAtCompany, getIndustryAverageSalary, getAverageSalaryByCompany} from './modules/workAroundModule.js';
